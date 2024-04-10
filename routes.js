15f2ad401fddb16c68bf8f0bbd1fbbb4502e9c47
Leticia Cristina <letici4cr1s@gmail.com>
2024-04-10 16:58:05 -0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,71 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Viagem } from './pages/dois';
+import { Filmes } from './pages/tres';
+import { Animais } from './pages/quatro';
+import { Receita } from './pages/um';
+import { Ionicons } from '@expo/vector-icons/'
+
+const Tab = createBottomTabNavigator();
+
+export function Routes() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="receita"
+                component={Receita}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="home" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="home-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="viagem"
+                component={Viagem}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="filmes"
+                component={Filmes}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="animais"
+                component={Animais}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
